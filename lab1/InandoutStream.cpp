241d00cc66993a63f6c94ddabf9ac5bd3fcdced4
grdphilip <philiprettig11@gmail.com>
2022-08-30 21:09:05 +0200

    Second commit

--- lab1/InandoutStream.cpp
+++ lab1/InandoutStream.cpp
@@ -5,8 +5,10 @@ using namespace std;
 int main()
 {
 
-int n1,n2,n3,n4,n5, n6;
+int n1,n2,n3,n4;
 float f1;
+char c;
+string word;
 
 // First output
 cout << "Enter one integer: " << endl;
@@ -16,18 +18,41 @@ cout << "You have entered the number: "
 
 // Second output
 cout << "Enter four integers: " << endl;
+cin >> n1;
 cin >> n2;
 cin >> n3;
 cin >> n4;
-cin >> n5;
 cout << "You have entered: "
-    << n2 << " " << n3 << " " << n4 << " " << n5 << endl;
+    << n1 << " " << n2 << " " << n3 << " " << n4 << endl;
 
+//Third output
 cout << "Enter one Integer and one real number: " << endl;
-cin >> n6;
+cin >> n1;
 cin >> f1;
-cout << "You have entered: "
-    << n6 << " " << f1 << endl;
+cout << "The real is: " << f1 << endl; 
+cout << "The Integer is: " << n1 << endl; 
+
+//Fourth output
+cout << "Enter a character" << endl;
+cin >> c;
+cout << "You entered "
+    << c << endl;
+    
+//Fifth output
+cout << "Enter a word: " << endl;
+cin >> word;
+cout << "The word " << word
+<< " has " << word.length() << " character(s)" << endl;
+
+//Sixth output
+cout << "Enter an integer and a word: " << endl;
+cin >> n1;
+cin >> word;
+cout << "You entered: "
+<< n1 << " and " << word << "." << endl;
+
+cout << "Enter a character and a word: " << endl;
+
 
 
 return 0;
