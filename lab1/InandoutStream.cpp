f27505a107d5678bf614f54d564cd7d114d07e17
grdphilip <philiprettig11@gmail.com>
2022-08-30 20:04:24 +0200

    First commit

---
+++ lab1/InandoutStream.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+int number;
+
+// First output
+cout << "Enter one integer: " << endl;
+cin >> number;
+cout << "You have entered the number: " 
+    << number << endl;
+
+cout << "Enter four integers: " << endl;
+
+
+return 0;
+}
--- lab1/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-
-int main()
-{
-cout << "A C++ program big boy" << endl;
-return 0;
-}
