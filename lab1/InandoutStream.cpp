e9653aded017133b86c8212e26898f6a8fd0b4da
grdphilip <philiprettig11@gmail.com>
2022-08-30 20:29:48 +0200

    Commit

--- lab1/InandoutStream.cpp
+++ lab1/InandoutStream.cpp
@@ -5,15 +5,29 @@ using namespace std;
 int main()
 {
 
-int number;
+int n1,n2,n3,n4,n5, n6;
+float f1;
 
 // First output
 cout << "Enter one integer: " << endl;
-cin >> number;
+cin >> n1;
 cout << "You have entered the number: " 
-    << number << endl;
+    << n1 << endl;
 
+// Second output
 cout << "Enter four integers: " << endl;
+cin >> n2;
+cin >> n3;
+cin >> n4;
+cin >> n5;
+cout << "You have entered: "
+    << n2 << " " << n3 << " " << n4 << " " << n5 << endl;
+
+cout << "Enter one Integer and one real number: " << endl;
+cin >> n6;
+cin >> f1;
+cout << "You have entered: "
+    << n6 << " " << f1 << endl;
 
 
 return 0;
