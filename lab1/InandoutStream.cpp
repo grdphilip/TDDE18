ff25c5bb61ae598366c8580a47818cc80e252ee9
grdphilip <philiprettig11@gmail.com>
2022-08-31 15:06:01 +0200

    Final commit

--- lab1/InandoutStream.cpp
+++ lab1/InandoutStream.cpp
@@ -1,60 +1,98 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 
 int main()
 {
 
-int n1,n2,n3,n4;
-float f1;
+int n;
+double f;
 char c;
 string word;
 
 // First output
-cout << "Enter one integer: " << endl;
-cin >> n1;
+cout << "Enter one integer: ";
+cin >> n;
 cout << "You have entered the number: " 
-    << n1 << endl;
+    << n << endl;
 
 // Second output
-cout << "Enter four integers: " << endl;
-cin >> n1;
-cin >> n2;
-cin >> n3;
-cin >> n4;
-cout << "You have entered: "
-    << n1 << " " << n2 << " " << n3 << " " << n4 << endl;
+cout << "Enter four integers: ";
+
+cin >> n;
+cout << "You have entered: ";
+cout << n << " ";
+cin >> n;
+cout << n << " ";
+cin >> n;
+cout <<  n << " ";
+cin >> n;
+cout << n << " " << endl;
+
 
 //Third output
-cout << "Enter one Integer and one real number: " << endl;
-cin >> n1;
-cin >> f1;
-cout << "The real is: " << f1 << endl; 
-cout << "The Integer is: " << n1 << endl; 
+cout << "Enter one integer and one real number: ";
+cin >> n;
+cin >> f;
+cout.precision(4);
+cout << "The real is: " << f << endl;
+cout << "The Integer is: " << n << endl;
+
+cout << "Enter one real and one integer number: ";
+cin >> f;
+cin >> n;
+cout << "The Integer is: " << n << endl;
+cout << "The real is: " << f << endl;
+
 
 //Fourth output
-cout << "Enter a character" << endl;
+cout << "Enter a character: " ;
 cin >> c;
-cout << "You entered "
+cout << "You entered: "
     << c << endl;
-    
+   
+
 //Fifth output
-cout << "Enter a word: " << endl;
+cout << "Enter a word: ";
 cin >> word;
-cout << "The word " << word
-<< " has " << word.length() << " character(s)" << endl;
+cout << "The word '" << word
+<< "' has " << word.length() << " character(s)" << endl;
 
 //Sixth output
-cout << "Enter an integer and a word: " << endl;
-cin >> n1;
+cout << "Enter an integer and a word: ";
+cin >> n;
 cin >> word;
-cout << "You entered: "
-<< n1 << " and " << word << "." << endl;
+cout << "You entered: "<< "'" << n << "'"<< " and " << "'" << word << "'" << endl;
 
-cout << "Enter a character and a word: " << endl;
+//Seventh output
+cout << "Enter an character and a word: ";
+cin >> c;
+cin >> word;
+cout << "You entered the word: "<< '"' << word << '"'<< " and the character " << "'" << c << "'" << endl;
+
+//Eight output
+cout << "Enter a word and a real number: " << endl;
+cin >> word;
+cin >> f;
+cin.ignore(1000, '\n');
+cout << "You entered "  << word << " and " << setprecision(3) << fixed << f << endl;
+
+//9th, 10th 11th
+cout << "Enter a text line: ";
+getline(cin, word);
+cout << "You entered: "<< '"' << word << '"'<< endl;
+
+cout << "Enter a second line of text: ";
+getline(cin, word);
+cout << "You entered: "<< '"' << word << '"'<< endl;
+
+cout << "Enter three words: ";
+getline(cin, word);
+cout << "You entered: "<< '"' << word << '"'<< endl;
 
 
 
 return 0;
-    //Adam was here
+
 }
