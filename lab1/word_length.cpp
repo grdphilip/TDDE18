a3076f5e7472369c2689399147e229779a5780be
grdphilip <philiprettig11@gmail.com>
2022-09-02 10:04:14 +0200

    Final commit lab1

--- lab1/word_length.cpp
+++ lab1/word_length.cpp
@@ -7,21 +7,35 @@ using namespace std;
 
 int main() {
 
-fstream myFile;
-string temp;
+ifstream myFile{"message.txt"};
+string temp{};
+string lword{};
+string sword{"kortaste"};
+int i{0};
+double sum{0};
+bool longer;
+
 
 //myFile.open("message.txt", ios::out); 
-myFile.open("message.txt",ios::in);
 
-if(myFile.is_open()) {
-    cout << "yes" << endl;
+while(myFile >> temp) {
+    
+    if(lword.length() < temp.length()) {
+        lword = temp;
+    } 
+
+    if(temp.length() < sword.length()){
+        sword = temp;
+    }
+
+  i++;
+  sum += double(temp.length());
 }
 
-while(getline(myFile, temp)) {
-    cout << temp << endl;
-}
-
-
+cout << "There are " << i << " words in the file." << endl;
+cout << "The shortest word was: " << '"' << sword << '"' <<  " with " << sword.length() << " character(s)" << endl;
+cout << "The longest word was: "  << '"' << lword << '"' <<  " with " << lword.length() << " character(s)" << endl;
+cout << "The average length was " << setprecision(3) << sum/i << " characters(s)" << endl;
 
 return 0;
 }
