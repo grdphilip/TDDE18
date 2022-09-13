773c550047c98593677e14e8572f1c6e02aab5bc
grdphilip <philiprettig11@gmail.com>
2022-09-13 19:35:30 +0200

    Edited

--- lab2/time.cpp
+++ lab2/time.cpp
@@ -4,21 +4,24 @@
 
 using namespace std;
 
-<<<<<<< HEAD
-bool is_am(Time const &t)
-{
-    return (t.HH < 12);
-=======
 bool is_valid(Time t) {
+
     if (t.hours > 23) {
+
         return false;
+
     } else if (t.minutes > 59) {
+
         return false;
+
     } else if (t.seconds > 59) {
+
         return false;
-    } 
+
+    }
+
     return true;
->>>>>>> 00c212c (Edit)
+
 }
 
 bool is_am(Time const &t)
