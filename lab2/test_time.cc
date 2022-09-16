c763d84a10dcb6bc2cbba8550f2a724d7090e50b
aodsvall <adaod351@liu.student.se>
2022-09-16 11:46:50 +0200

    lab2 DONE.

--- lab2/test_time.cc
+++ lab2/test_time.cc
@@ -1,62 +1,169 @@
 #include <iostream>
 #include "catch.hpp"
 #include "time.h"
-
-
+#include <sstream>
 
 using namespace std;
 
+TEST_CASE("Is valid")
+{
 
+    Time t{00, 00, 00};
 
-TEST_CASE("Is valid") {
-    
-    Time t{0,0,0};
-
-CHECK(is_valid(t) == true);
-
+    CHECK(is_valid(t) == true);
 }
 
-TEST_CASE("Is am") {
-    Time t{11,59,59};
+TEST_CASE("Is am")
+{
+    Time t{11, 59, 59};
 
     CHECK(is_am(t) == true);
 }
 
-TEST_CASE("Operator ==") {
-    Time t1{11,11,11};
-    Time t2{11,11,11};
+TEST_CASE("to string 24 hour time")
+{
+    Time t1{22, 23, 23};
 
-    CHECK(t1==t2);
-}
- 
-TEST_CASE("Operator!=") {
-    Time t1{12,11,11};
-    Time t2{11,11,11};
-
-    CHECK(t1!=t2);
+    CHECK(to_string(t1, false) == "22:23:23");
 }
 
-TEST_CASE("Operator +") {
-    Time t1{23,59,59};
-    Time t2{0,0,0};
+TEST_CASE("to string regular time// am")
+{
+    Time t1{10, 10, 23};
 
-    CHECK(t1+1 == t2);
+    CHECK(to_string(t1, true) == "10:10:23[am]");
 }
 
-TEST_CASE("Operator ++x") {
-    Time t1{23,59,59};
-    Time t2{0,0,0};
+TEST_CASE("to string regular time//pm")
+{
+    Time t1{20, 06, 00};
+
+    CHECK(to_string(t1, true) == "08:06:00[pm]");
+}
+
+TEST_CASE("Operator ==")
+{
+    Time t1{11, 11, 11};
+    Time t2{11, 11, 11};
+
+    CHECK(t1 == t2);
+}
+
+TEST_CASE("Operator!=")
+{
+    Time t1{12, 11, 11};
+    Time t2{11, 11, 11};
+
+    CHECK(t1 != t2);
+}
+
+TEST_CASE("Operator +")
+{
+    Time t1{23, 59, 59};
+    Time t2{01, 00, 00};
+
+    CHECK(t1 + 3601 == t2);
+}
+
+TEST_CASE("Operator ++x")
+{
+    Time t1{23, 59, 59};
+    Time t2{00, 00, 00};
 
     CHECK(++t1 == t2);
 }
 
-TEST_CASE("Operator x++") {
-    Time t1{12,0,0};
-    Time t2{12,0,0};
-  
+TEST_CASE("Operator x++")
+{
+    Time t1{12, 00, 00};
+    Time t2{12, 00, 01};
+
     CHECK(t1++ == t2);
 }
 
+TEST_CASE("Operator -")
+{
+    Time t1{00, 00, 00};
+    Time t2{23, 59, 59};
 
+    CHECK(t1 - 1 == t2);
+}
+
+TEST_CASE("Operator --x")
+{
+    Time t1{00, 00, 00};
+    Time t2{23, 59, 59};
+
+    CHECK(--t1 == t2);
+}
+
+TEST_CASE("Operator x--")
+{
+    Time t1{00, 00, 00};
+    Time t2{23, 59, 59};
+
+    CHECK(t1-- == t2);
+}
+
+TEST_CASE("Operator >")
+{
+    Time t1{11, 15, 20};
+    Time t2{11, 15, 19};
+
+    CHECK(t1 > t2);
+}
+
+TEST_CASE("Operator <")
+{
+    Time t1{11, 15, 19};
+    Time t2{11, 15, 20};
+
+    CHECK(t1 < t2);
+}
+
+TEST_CASE("Operator >=")
+{
+    Time t1{11, 11, 12};
+    Time t2{11, 11, 11};
+
+    CHECK(t1 >= t1);
+
+    t1 = {11, 11, 11};
+    t2 = {11, 11, 11};
+
+    CHECK(t1 >= t2);
+}
+
+TEST_CASE("Operator <=")
+{
+    Time t1{11, 11, 11};
+    Time t2{11, 11, 12};
+
+    CHECK(t1 <= t2);
+
+    t1 = {11, 11, 11};
+    t2 = {11, 11, 11};
+
+    CHECK(t1 <= t2);
+}
+
+TEST_CASE("Operator <<")
+{
+    Time t1{23, 16, 18};
+    ostringstream out{};
+    out << t1;
+
+    CHECK(out.str() == to_string(t1, 0));
+}
+
+TEST_CASE("Operator >>") {
+    Time t1{};
+    stringstream in{"23:40:41"};
+    in >> t1;
+
+    CHECK(t1.HH == 23);
+    CHECK(t1.MM == 40);
+    CHECK(t1.SS == 41);
+}
 
 
--- lab2/time.cpp
+++ lab2/time.cpp
@@ -1,96 +1,205 @@
 #include <iostream>
 #include "time.h"
+#include <sstream>
+#include <iomanip>
 
 using namespace std;
 
-bool is_valid(Time const t) {
+bool is_valid(Time const time)
+{
 
-    if (t.HH > 23) {
-
-        return false;
-
-    } else if (t.MM > 59) {
+    if (time.HH > 23 || time.HH < 0)
+    {
 
         return false;
-
-    } else if (t.SS > 59) {
+    }
+    else if (time.MM > 59 || time.MM < 0)
+    {
 
         return false;
+    }
+    else if (time.SS > 59 || time.SS < 0)
+    {
 
+        return false;
     }
 
     return true;
-
 }
 
-bool is_am(Time const &t)
+bool is_am(Time const &time)
 {
-    return (t.HH < 12);
-}
-
-bool operator ==(Time const& a, Time const& b) {
-    while(a.HH == b.HH and a.MM == b.MM and a.SS == b.SS) {
+    if (time.HH < 12)
+    {
         return true;
-    }  
+    }
     return false;
 }
 
+std::string to_string(Time const &time, bool regular_time)
+{
+    std::ostringstream out{};
 
-bool operator !=(Time const& a, Time const& b) {
-    return !(a==b);
+    out << setw(2) << setfill('0');
+    if (regular_time)
+    {
+        if (time.HH == 0)
+        {
+            out << 12;
+        }
+        else if (is_am(time) || time.HH == 12)
+        {
+            out << time.HH;
+        }
+        else
+        {
+
+            out << time.HH - 12;
+        }
+    }
+    else
+    {
+        out << time.HH;
+    }
+    out << ":" << setw(2) << time.MM << ":" << setw(2) << time.SS;
+    if (regular_time)
+    {
+        out << "[";
+        if (is_am(time))
+        {
+            out << "am";
+        }
+        else
+        {
+            out << "pm";
+        }
+        out << "]";
+    }
+    return out.str();
 }
 
-Time& operator +(Time &t, int sec) {
-    
-    t.SS = t.SS + sec;
-   
-     while (t.SS > 59) {
-        t.MM  +=1;
-        t.SS -=60;
+bool operator==(Time const &time1, Time const &time2)
+{
+    while (time1.HH == time2.HH && time1.MM == time2.MM && time1.SS == time2.SS)
+    {
+        return true;
+    }
+    return false;
+}
 
-    if(t.MM > 59) {
-            t.HH +=1;
-            t.MM -= 60;
+bool operator!=(Time const &time1, Time const &time2)
+{
+    return !(time1 == time2);
+}
 
-        if (t.HH > 23) {
-        t.HH-=24;
-    }  
+Time &operator+(Time &time, int sec)
+{
+
+    time.SS = time.SS + sec;
+
+    while (time.SS > 59)
+    {
+        time.MM += 1;
+        time.SS -= 60;
+
+        while (time.MM > 59)
+        {
+            time.HH += 1;
+            time.MM -= 60;
+
+            if (time.HH > 23)
+            {
+                time.HH -= 24;
+            }
         }
     }
 
-    return t;
+    return time;
 }
 
-Time& operator ++(Time& t) {
-    t.SS = t.SS + 1;
-   
-    while (t.SS > 59) {
-        t.MM  +=1;
-        t.SS -=60;
+Time &operator++(Time &time)
+{
+    time + 1;
+    return time;
+}
 
-    if(t.MM > 59) {
-            t.HH +=1;
-            t.MM -= 60;
+Time operator++(Time &time, int)
+{
+    time = time + 1;
+    return time;
+}
 
-        if (t.HH > 23) {
-        t.HH-=24;
-    }  
+Time &operator-(Time &time, int sec)
+{
+    time.SS = time.SS - sec;
+
+    while (time.SS < 0)
+    {
+        time.MM -= 1;
+        time.SS += 60;
+        while (time.MM < 0)
+        {
+            time.HH -= 1;
+            time.MM += 60;
+            if (time.HH < 0)
+            {
+                time.HH += 24;
+            }
         }
     }
-
-    return t;
+    return time;
 }
 
-Time operator ++(Time& t, int) {
-    Time tmp = t;
-    t = t+1;
-    return tmp;
+Time &operator--(Time &time)
+{
+    time - 1;
+    return time;
 }
 
+Time operator--(Time &time, int sec)
+{
+    time - 1;
+    return time;
+}
 
+bool operator>(Time const &time1, Time const &time2)
+{
+    return (time1.HH > time2.HH || time1.HH == time2.HH && time1.MM > time2.MM || time1.MM == time2.MM && time1.SS > time2.SS);
+}
 
+bool operator<(Time const &time1, Time const &time2)
+{
+   return(time2 > time1);
+}
 
+bool operator>=(Time const &time1, Time const &time2)
+{
+    return (time1 > time2 || time1 == time2);
+}
 
+bool operator<=(Time const &time1, Time const &time2)
+{
+    return (time2 >= time1);
+}
 
+std::ostream &operator<<(std::ostream &os, Time const &time)
+{
+    os << to_string(time, 0);
+    return os;
+}
 
-
+istream &operator>>(istream &is, Time &time)
+{
+    char ignore_colon;
+    Time tmp{time};
+    is >> tmp.HH >> ignore_colon >> tmp.MM >> ignore_colon >> tmp.SS;
+    if (!is_valid(tmp) || ignore_colon != ':')
+    {
+        is.setstate(ios::failbit);
+    }
+    else
+    {
+        time = tmp;
+    }
+    return is;
+}
