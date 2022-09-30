64d696623b40b3f7c77b7ff5c328d95ea4d6979a
Philip Rettig <phire844@student.liu.se>
2022-09-30 15:28:10 +0200

    Comp finished

--- lab2/time.h
+++ lab2/time.h
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <sstream>
 
 struct Time
 {
@@ -8,18 +12,18 @@ struct Time
 
 bool is_valid(Time time);
 bool is_am(Time const &time);
-std::string to_string(Time const& time, bool regular_time);
+std::string to_string(Time const &time, bool regular_time);
 bool operator==(Time const &time1, Time const &time2);
 bool operator!=(Time const &time1, Time const &time2);
-Time operator+(Time &time, int sec);
+Time operator+(Time const &time, int sec);
 Time &operator++(Time &time);
 Time operator++(Time &time, int);
-Time operator-(Time &time, int sec);
+Time operator-(Time const &time, int sec);
 Time &operator--(Time &time);
 Time operator--(Time &time, int sec);
-bool operator >(Time const& time1, Time const& time2);
-bool operator <(Time const& time1, Time const& time2);
-bool operator >=(Time const& time1, Time const& time2);
-bool operator <=(Time const& time1, Time const& time2);
-std::ostream& operator <<(std::ostream& os, Time const& time);
-std::istream& operator >>(std::istream& is, Time& time);
+bool operator>(Time const &time1, Time const &time2);
+bool operator<(Time const &time1, Time const &time2);
+bool operator>=(Time const &time1, Time const &time2);
+bool operator<=(Time const &time1, Time const &time2);
+std::ostream &operator<<(std::ostream &os, Time const &time);
+std::istream &operator>>(std::istream &is, Time &time);
