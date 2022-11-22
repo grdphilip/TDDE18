ac6779230a5fb2cdc8810733400abc3af74b7ded
aodsvall <adaod351@student.liu.se>
2022-11-22 10:23:33 +0100

    DONE

--- lab4/main.cc
+++ lab4/main.cc
@@ -8,7 +8,7 @@
 
 
 using namespace std;
-//void deallocate_components(vector <Component*> vec);
+void deallocate_components(vector <Component*> vec);
 void circuit_builder1(int const iterations, int const lines, double const time,  double const voltage) {
     vector<Component*> vec{};
 
@@ -17,19 +17,14 @@ void circuit_builder1(int const iterations, int const lines, double const time, 
     Terminal r124{};
     Terminal r32{};
 
-    Battery b{"Bat",voltage,n,p};
-    Resistor r1{"R1",6,p,r124};
-    Resistor r2{"R2", 4,r124,r32};
-    Resistor r3{"R3",8,r32,n};
-    Resistor r4{"R4",12,r124,n};
-    vec.push_back(&b);
-    vec.push_back(&r1);
-    vec.push_back(&r2);
-    vec.push_back(&r3);
-    vec.push_back(&r4);
+    vec.push_back(new Battery("Bat",voltage,n,p));
+    vec.push_back(new Resistor("R1",6,p,r124));
+    vec.push_back(new Resistor("R2",4,r124,r32));
+    vec.push_back(new Resistor("R3",8,r32,n));
+    vec.push_back(new Resistor("R4",12,r124,n));
     Simulator s{vec,iterations,lines,time};
     s.simulate();
-    //deallocate_components(vec);
+    deallocate_components(vec);
 }
 void circuit_builder2(int const iterations, int const lines, double const time,  double const voltage) {
     vector<Component*> vec{};
@@ -39,21 +34,15 @@ void circuit_builder2(int const iterations, int const lines, double const time, 
     Terminal L{};
     Terminal R{};
 
-    Battery b{"Bat",voltage,n,p};
-    Resistor r1{"R1",150,p,L};
-    Resistor r2{"R2",50,p,R};
-    Resistor r3{"R3",100, L, R};
-    Resistor r4{"R4",300,L, n};
-    Resistor r5{"R5", 250, R, n};
-    vec.push_back(&b);
-    vec.push_back(&r1);
-    vec.push_back(&r2);
-    vec.push_back(&r3);
-    vec.push_back(&r4);
-    vec.push_back(&r5);
+    vec.push_back(new Battery("Bat",voltage,n,p));
+    vec.push_back(new Resistor("R1",150,p,L));
+    vec.push_back(new Resistor("R2",50,p,R));
+    vec.push_back(new Resistor("R3",100,L,R));
+    vec.push_back(new Resistor("R4",300,L,n));
+    vec.push_back(new Resistor("R5",250,R,n));
     Simulator s{vec,iterations,lines,time};
     s.simulate();
-    //deallocate_components(vec);
+    deallocate_components(vec);
 
 }
 void circuit_builder3(int const iterations, int const lines, double const time,  double const voltage) {
@@ -70,22 +59,23 @@ void circuit_builder3(int const iterations, int const lines, double const time, 
     Capacitor c3{"C3",1, L, R};
     Resistor r4{"R4",300,L, n};
     Capacitor c5{"C5", 0.75, R, n};
-    vec.push_back(&b);
-    vec.push_back(&r1);
-    vec.push_back(&r2);
-    vec.push_back(&c3);
-    vec.push_back(&r4);
-    vec.push_back(&c5);
+
+    vec.push_back(new Battery("Bat",voltage,n,p));
+    vec.push_back(new Resistor("R1",150,p,L));
+    vec.push_back(new Resistor("R2",50,p,R));
+    vec.push_back(new Capacitor("C3",1,L,R));
+    vec.push_back(new Resistor("R4",300,L,n));
+    vec.push_back(new Capacitor("C5",0.75,R,n));
     Simulator s{vec,iterations,lines,time};
     s.simulate();
-    //deallocate_components(vec);
+    deallocate_components(vec);
 }
-/*
+
 void deallocate_components(vector<Component*> vec) {
     for(Component* i: vec) {
         delete i;
     }
-}*/
+}
 
 int main(int argc, char** argv){
     
