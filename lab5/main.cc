ed47cd76862fe758f99f04506e8e8eb0c0141ae9
Philip Rettig <phire844@student.liu.se>
2022-12-07 11:28:04 +0100

    Hello

--- lab5/main.cc
+++ lab5/main.cc
@@ -24,27 +24,57 @@ int main(int argc, char **argv)
     ifstream textFile{inFile};
     vector<string> text{};
     copy(istream_iterator<string>(textFile), istream_iterator<string>(), back_insert_iterator<vector<string>>(text));
-    map<string, int> wordmap{}; // Defaults in ascending order
+    map<string, int> wordmap{}; // Defaults in ascending order, therefor good alternative for table
     vector<string> args(argv + 2, argv + argc);
+
+
+    auto print = [](std::pair<const string, int>& itp) {  cout << " " << itp.first << " " << itp.second << endl;    };
+
 
     if (args[0] == "--print")
     {
         copy(text.begin(), text.end(), ostream_iterator<string>(cout, " "));
     }
-    else if (args[0] == "--frequency")
+    else if (args[0] == "--table")
     {
+
+        auto print = [](std::pair<const string, int>& itp) {  cout << " " << itp.first << " " << itp.second << endl;};
+
         for_each(text.begin(), text.end(), [&wordmap](std::string s)
                  { return wordmap[s] += 1; });
 
-        for_each(wordmap.begin(), wordmap.end(), [](const pair<string, int> &itp)
-                 { cout << itp.first << ": " << itp.second << endl; });
+        for_each(wordmap.begin(),wordmap.end(),print);  
+    
+    } else if (args[0] == "--frequency")
+    {
+        vector<pair<string,int>> freqency{};
+
+
+
+
+
+        
+        
+
+    }
+
+    
+
+        
+
+                
+
 
         // copy(wordmap.cbegin(), wordmap.cend(), ostream_iterator<string, int>(cout, " "));
         /*
+
+        copy(wordmap.begin(), wordmap.end(), [](const pair<string, int> &itp)
+                 { cout << itp.first << ": " << itp.second << endl; });
+
         for (auto it = wordmap.cbegin(); it != wordmap.cend(); ++it)
         {
             std::cout << it->first << " " << it->second << endl;
         }
         */
-    }
+    
 }
